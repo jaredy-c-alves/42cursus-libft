4e694acad69879b80439600de138c1019239927b
Jaredy Alves <94395541+jaredy-c-alves@users.noreply.github.com>
2022-11-24 18:16:10 +0000

    Adding ft_lstclear.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jcapistr <jcapistr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 21:56:48 by jcapistr          #+#    #+#             */
-/*   Updated: 2022/11/24 18:07:21 by jcapistr         ###   ########.fr       */
+/*   Updated: 2022/11/24 18:13:14 by jcapistr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,5 +63,6 @@ int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
 
 #endif //LIBFT_H
