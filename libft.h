cecac87e929fcf0e21277a4b2fa40268b87903b3
Jaredy Alves <94395541+jaredy-c-alves@users.noreply.github.com>
2022-11-24 11:34:43 +0000

    Adding ft_putchar_fd.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jcapistr <jcapistr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 21:56:48 by jcapistr          #+#    #+#             */
-/*   Updated: 2022/11/24 11:21:01 by jcapistr         ###   ########.fr       */
+/*   Updated: 2022/11/24 11:30:42 by jcapistr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -45,5 +46,6 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_putchar_fd(char c, int fd);
 
 #endif //LIBFT_H
