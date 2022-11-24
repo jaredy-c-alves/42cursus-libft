8ff96c9adfee8f87cc19fb562e559ef758386688
Jaredy Alves <94395541+jaredy-c-alves@users.noreply.github.com>
2022-11-24 11:26:48 +0000

    Adding ft_striteri.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jcapistr <jcapistr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 21:56:48 by jcapistr          #+#    #+#             */
-/*   Updated: 2022/11/24 11:14:51 by jcapistr         ###   ########.fr       */
+/*   Updated: 2022/11/24 11:21:01 by jcapistr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 
 #endif //LIBFT_H
