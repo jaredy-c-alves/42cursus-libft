acf35e9fb626caf3f31b5f4b8da91553fc8a6823
Jaredy Alves <94395541+jaredy-c-alves@users.noreply.github.com>
2022-11-24 11:13:58 +0000

    Fixing ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,83 +6,57 @@
 /*   By: jcapistr <jcapistr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 21:57:04 by jcapistr          #+#    #+#             */
-/*   Updated: 2022/11/21 21:57:04 by jcapistr         ###   ########.fr       */
+/*   Updated: 2022/11/24 11:13:04 by jcapistr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_isneg(int *n, int *neg, int *tmp)
+static int	ft_abs(int n)
 {
-	if (*n == -2147483648)
-	{
-		*n = *n + 1;
-		*neg = -1;
-		*tmp = 1;
-		*n = *n * -1;
-	}
-	else if (*n < 0)
-	{
-		*neg = -1;
-		*n = *n * -1;
-		*tmp = 0;
-	}
-	else if (*n >= 0)
-	{
-		*neg = 1;
-		*tmp = 0;
-	}
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
 }
 
-static int	ft_itoa_len(int n)
+static void	ft_strrev(char *str)
 {
-	int	len;
+	size_t	i;
+	size_t	len;
+	char	tmp;
 
-	len = 0;
-	while (n > 9)
+	i = 0;
+	len = ft_strlen(str);
+	while (i < len / 2)
 	{
-		n = n / 10;
-		len++;
+		tmp = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = tmp;
+		i++;
 	}
-	len++;
-	return (len);
-}
-
-static void	ft_itoa_write(char *str, int len, int n, int tmp)
-{
-	while (n > 9)
-	{
-		str[len--] = (char)((n % 10) + '0' + tmp);
-		n = n / 10;
-		tmp = 0;
-	}
-	str[len] = n + '0';
 }
 
 char	*ft_itoa(int n)
 {
+	size_t	len;
 	int		neg;
-	int		tmp;
-	int		len;
 	char	*str;
 
-	ft_isneg(&n, &neg, &tmp);
-	len = ft_itoa_len(n);
-	if (neg == -1)
+	len = 0;
+	neg = (n < 0);
+	str = ft_calloc((10 + neg + 1), sizeof(*str));
+	if (!str)
+		return (0);
+	if (n == 0)
+		str[0] = '0';
+	while (n != 0)
 	{
-		str = malloc((len + 2) * sizeof(char));
-		if (!str)
-			return (0);
-		len++;
-		str[0] = '-';
+		str[len++] = (char)('0' + ft_abs(n % 10));
+		n /= 10;
 	}
-	else
-	{
-		str = malloc((len + 1) * sizeof(char));
-		if (!str)
-			return (0);
-	}
-	str[len--] = '\0';
-	ft_itoa_write(str, len, n, tmp);
+	if (neg)
+		str[len] = '-';
+	ft_strrev(str);
 	return (str);
 }
