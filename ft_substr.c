32c0b607faa96f107cbc71e168351ed4e42a47f3
Jaredy Alves <94395541+jaredy-c-alves@users.noreply.github.com>
2022-11-24 23:42:43 +0000

    Fixing ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jcapistr <jcapistr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 22:00:14 by jcapistr          #+#    #+#             */
-/*   Updated: 2022/11/21 22:00:14 by jcapistr         ###   ########.fr       */
+/*   Updated: 2022/11/24 23:41:19 by jcapistr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,19 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*d;
-	size_t	s_len;
-	size_t	end;
+	char	*ptr;
+	size_t	n_len;
 
 	if (!s)
 		return (0);
-	s_len = ft_strlen(s);
-	end = 0;
-	if (start < s_len)
-		end = s_len - start;
-	if (end > len)
-		end = len;
-	d = (char *)malloc(sizeof(char) * (end + 1));
-	if (!d)
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	n_len = ft_strlen(s + start);
+	if (n_len < len)
+		len = n_len;
+	ptr = (char *)malloc((len + 1) * sizeof(char));
+	if (!ptr)
 		return (0);
-	ft_strlcpy(d, s + start, end + 1);
-	return (d);
+	ft_strlcpy(ptr, s + start, len + 1);
+	return (ptr);
 }
