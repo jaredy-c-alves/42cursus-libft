01bcebc9db1f67ecdacb8a70d89b4d4ca7111d23
Jaredy Alves <94395541+jaredy-c-alves@users.noreply.github.com>
2022-11-24 18:41:10 +0000

    Adding ft_lstmap.c

--- Makefile
+++ Makefile
@@ -16,7 +16,7 @@ OBJS	= $(SRCS:.c=.o)
 
 BSRCS	= \
 ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c ft_lstadd_back.c \
-ft_lstdelone.c ft_lstclear.c ft_lstiter.c
+ft_lstdelone.c ft_lstclear.c ft_lstiter.c ft_lstmap.c
 BOBJS	= $(BSRCS:.c=.o)
 
 %.o:
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jcapistr <jcapistr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 21:56:48 by jcapistr          #+#    #+#             */
-/*   Updated: 2022/11/24 18:16:53 by jcapistr         ###   ########.fr       */
+/*   Updated: 2022/11/24 18:28:22 by jcapistr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,5 +65,7 @@ void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
 
 #endif //LIBFT_H
