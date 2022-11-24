838edf734d1023ffe90bf64d32f5af625634e18d
Jaredy Alves <94395541+jaredy-c-alves@users.noreply.github.com>
2022-11-24 18:06:18 +0000

    Fixing ft_lstadd_front.c

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: jcapistr <jcapistr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 17:31:58 by jcapistr          #+#    #+#             */
-/*   Updated: 2022/11/24 17:45:32 by jcapistr         ###   ########.fr       */
+/*   Updated: 2022/11/24 18:04:58 by jcapistr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (new)
 	{
+		if (!*lst)
+		{
+			*lst = new;
+			return ;
+		}
 		new->next = *lst;
 		*lst = new;
 	}
