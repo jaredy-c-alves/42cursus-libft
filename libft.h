999b5324388cb0236761a4a0a00d99401fd1d0ce
Jaredy Alves <94395541+jaredy-c-alves@users.noreply.github.com>
2022-11-24 18:11:45 +0000

    Adding ft_lstdelone.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jcapistr <jcapistr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 21:56:48 by jcapistr          #+#    #+#             */
-/*   Updated: 2022/11/24 18:01:03 by jcapistr         ###   ########.fr       */
+/*   Updated: 2022/11/24 18:07:21 by jcapistr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,6 @@ void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif //LIBFT_H
