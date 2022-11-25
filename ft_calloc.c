3e81e5f8ac64c8c7f15d82f555aa26bcf31fb8c1
jcapistr <94395541+jaredy-c-alves@users.noreply.github.com>
2022-11-25 10:11:57 +0000

    Fixing ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,17 +6,20 @@
 /*   By: jcapistr <jcapistr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 21:57:45 by jcapistr          #+#    #+#             */
-/*   Updated: 2022/11/24 22:08:15 by jcapistr         ###   ########.fr       */
+/*   Updated: 2022/11/25 10:13:14 by jcapistr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	unsigned char	*ptr;
 	size_t			i;
 
+	if (nmemb >= SSIZE_MAX || size >= SSIZE_MAX)
+		return (0);
 	i = 0;
 	ptr = (unsigned char *)malloc(nmemb * size);
 	if (!ptr)
